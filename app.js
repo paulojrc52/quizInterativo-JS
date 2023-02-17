1359e7df0624a9bfd8af0d3f6de6cdae39b2b489
Paulo Júnior <paulojrcosta300@gmail.com>
2023-02-17 18:38:17 -0300

    Rolagem e animação (scrollTo)/(setInterval)

--- app.js
+++ app.js
@@ -20,6 +20,18 @@ form.addEventListener('submit', event => {
     }
   })
 
+  scrollTo(0, 0)
+
   finalResult.querySelector('span').textContent = `${score}%`
   finalResult.classList.remove('d-none')
+  let counter = 0
+
+  const timer = setInterval(() => {
+    if(counter === score) {
+      clearInterval(timer)
+    }
+
+    finalResult.querySelector('span').textContent = `${counter}%`
+    counter++
+  }, 10)
 })
