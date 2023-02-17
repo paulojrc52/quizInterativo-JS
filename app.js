7d87dfc305bddda2ca48d53f0dcb611f86ce8c22
Paulo Júnior <paulojrcosta300@gmail.com>
2023-02-17 18:34:43 -0300

    Respostas corretas, verificando e declarando o score do user

---
+++ app.js
@@ -0,0 +1,25 @@
+const form = document.querySelector('.quiz-form')
+const finalResult = document.querySelector('.result')
+
+const correctAnswers = ['B', 'B', 'B', 'B']
+
+form.addEventListener('submit', event => {
+  event.preventDefault()
+
+  let score = 0
+  const userAnswers = [
+    form.inputQuestion1.value,
+    form.inputQuestion2.value,
+    form.inputQuestion3.value,
+    form.inputQuestion4.value,
+  ]
+
+  userAnswers.forEach((userAnswer, index) => {
+    if(userAnswer === correctAnswers[index]) {
+      score += 25
+    }
+  })
+
+  finalResult.querySelector('span').textContent = `${score}%`
+  finalResult.classList.remove('d-none')
+})
